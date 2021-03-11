c8948cae7e516abef59a1c8b0a0632af1efb00df
Andres Fajardo <andresf.f321@yahoo.comgit config user.emailgit config --global user.email andresf.f321@yahoo.com>
2021-03-11 13:47:01 -0800

    added the ability to save tables

--- server.js
+++ server.js
@@ -8,9 +8,25 @@ const PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const tables = [
+
+]
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'home.html')));
 app.get('/reservation', (req, res) => res.sendFile(path.join(__dirname, 'reservation.html')));
 app.get('/tables', (req, res) => res.sendFile(path.join(__dirname, 'tablesAvailable.html')));
+
+app.post('/api/tables', (req, res) => {
+   
+    const newguest = req.body;
+  
+
+    
+    console.log(newguest);
+  
+   tables.push(newguest);
+    res.json(newguest);
+  });
+app.get('/api/tables', (req, res) => res.json(tables));
 
 app.listen(PORT, () => console.log(`Server is listening on PORT: ${PORT}`));
